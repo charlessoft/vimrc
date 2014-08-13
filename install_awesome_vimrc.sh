e07f5f70f4eecf2cab829a4c06defa755df1afff
charlessoft <charlessoft@qq.com>
2014-08-13 20:31:29 +0800

    add python synax

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -8,6 +8,7 @@ source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
 source ~/.vim_runtime/vimrcs/charles.vim
 source ~/.vim_runtime/vimrcs/matrix.vim
+source ~/.vim_runtime/vimrcs/python.vim
 
 try
 source ~/.vim_runtime/my_configs.vim
